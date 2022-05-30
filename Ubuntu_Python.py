c619c6223ee315ac0e609b29c8d4a35ea0f769da
APRICITY-NY <104713295+APRICITY-NY@users.noreply.github.com>
2022-05-30 10:59:34 +0900

    Add files via upload

---
+++ Ubuntu_Python.py
@@ -0,0 +1,19 @@
+def add_two(a, b):
+    return a + b
+
+def subtract_two(a, b):
+    if a >= b:
+        return a - b
+    else:
+        return b - a
+
+def multiple_two(a, b):
+    return a * b
+
+def division_two(a, b):
+    return a // b
+
+print(add_two(34,56))
+print(subtract_two(54, 90))
+print(multiple_two(4,5))
+print(division_two(100,50))
